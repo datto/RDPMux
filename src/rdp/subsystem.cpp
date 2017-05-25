fc87d35d02fa07e57c7eddcfb564548780a994c5
Sri Ramanujam <sramanujam@datto.com>
2017-05-24 20:10:04 -0400

    Proper monitor resizing in subsystem

--- src/rdp/subsystem.cpp
+++ src/rdp/subsystem.cpp
@@ -45,7 +45,7 @@ int rdpmux_subsystem_process_message(rdpmuxShadowSubsystem *system, wMessage *me
 	    shadow_subsystem_frame_update((rdpShadowSubsystem *) system);
 	    break;
 	default:
-        WLog_ERR(TAG, "Unprocessed message: %u", message->id);
+        WLog_WARN(TAG, "Unprocessed message: %u", message->id);
         break;
     }
 
@@ -66,18 +66,15 @@ void rdpmux_subsystem_update_frame(rdpmuxShadowSubsystem *system, BOOL full)
     if (ArrayList_Count(server->clients) < 1) {
         return;
     }
-    printf("Trying to update\n");
+
     auto formats = system->listener->GetRDPFormat();
     auto source_format = std::get<0>(formats);
     auto dest_format = std::get<1>(formats);
     auto source_bpp = std::get<2>(formats);
 
     if (source_format < 0) {
-        printf("Invalid source format\n");
         return; // invalid buffer type, don't make the copy
     }
-
-    printf("source format: 0x%08x, dest format: 0x%08x, source bpp: %d\n", source_format, dest_format, source_bpp);
 
     // for now, force fulls no matter what
 
@@ -86,14 +83,10 @@ void rdpmux_subsystem_update_frame(rdpmuxShadowSubsystem *system, BOOL full)
     invalidRect.right = (UINT16) system->listener->GetWidth();
     invalidRect.bottom = (UINT16) system->listener->GetHeight();
 
-    printf("Invalid rect: %dx%d (%dx%d)\n", invalidRect.left, invalidRect.top, invalidRect.right, invalidRect.bottom);
-
     surfaceRect.top = 0;
     surfaceRect.left = 0;
     surfaceRect.right = (UINT16) surface->width;
     surfaceRect.bottom = (UINT16) surface->height;
-
-    printf("Surface rect: %dx%d (%dx%d)\n", surfaceRect.left, surfaceRect.top, surfaceRect.right, surfaceRect.bottom);
 
     region16_union_rect(&(surface->invalidRegion), &(surface->invalidRegion), &invalidRect);
     region16_intersect_rect(&(surface->invalidRegion), &(surface->invalidRegion), &surfaceRect);
@@ -105,9 +98,7 @@ void rdpmux_subsystem_update_frame(rdpmuxShadowSubsystem *system, BOOL full)
         auto y = extents->top;
         auto width = extents->right - extents->left;
         auto height = extents->bottom - extents->top;
-
-        printf("Debug: %d %d %d %d\n", x, y, width, height);
-        WLog_INFO(TAG, "Invalid region: x = %d, y = %d, width = %d, height = %d", x, y, width, height);
+        WLog_DBG(TAG, "Invalid region: x = %d, y = %d, width = %d, height = %d", x, y, width, height);
 
 
         freerdp_image_copy(surface->data,                             /* destination surface */
@@ -119,7 +110,7 @@ void rdpmux_subsystem_update_frame(rdpmuxShadowSubsystem *system, BOOL full)
                            extents->bottom - extents->top,            /* y coordinate of bottom right corner of region to copy */
                            (BYTE *) system->listener->shm_buffer,     /* source surface to copy data from */
                            source_format,                             /* source surface pixel format */
-                           system->listener->GetWidth() * source_bpp, /* scanline of source surface */
+                           system->src_width * source_bpp,            /* scanline of source surface */
                            extents->left,                             /* x coord of top left corner of dirty part of source buffer */
                            extents->top,                              /* y coord of top left corner of dirty part of source buffer */
                            NULL,                                      /* GDI palette to use */
@@ -127,67 +118,7 @@ void rdpmux_subsystem_update_frame(rdpmuxShadowSubsystem *system, BOOL full)
         );
         shadow_subsystem_frame_update((rdpShadowSubsystem *) system);
         region16_clear(&(surface->invalidRegion));
-    } else {
-        printf("Debug: empty extents\n");
     }
-}
-
-void *rdpmux_subsystem_thread(rdpmuxShadowSubsystem *system)
-{
-    DWORD nCount = 0;
-    HANDLE events[32];
-    HANDLE stopEvent = system->server->StopEvent;
-    wMessagePipe *msgPipe = system->MsgPipe;
-    wMessage message;
-    DWORD interval;
-    UINT64 frametime;
-
-    events[nCount++] = stopEvent;
-    events[nCount++] = MessageQueue_Event(msgPipe->In);
-
-    system->captureFrameRate = 30;
-    interval = (DWORD) (1000 / system->captureFrameRate);
-    frametime = GetTickCount64() + interval;
-
-    while(1) {
-        WaitForMultipleObjects(nCount, events, FALSE, 0);
-
-        if (WaitForSingleObject(stopEvent, 0) == WAIT_OBJECT_0) {
-            WLog_INFO(TAG, "Server is stopping");
-            break;
-        }
-
-        if (WaitForSingleObject(MessageQueue_Event(msgPipe->In), 0) == WAIT_OBJECT_0) {
-            if (MessageQueue_Peek(msgPipe->In, &message, TRUE)) {
-                if (message.id == WMQ_QUIT) {
-                    break;
-                }
-                rdpmux_subsystem_process_message(system, &message);
-            }
-        }
-
-        if (GetTickCount64() >= frametime)
-            rdpmux_subsystem_update_frame(system, FALSE);
-    }
-
-    return NULL;
-}
-
-int rdpmux_subsystem_start(rdpmuxShadowSubsystem *system)
-{
-    if (!system)
-        return -1;
-
-    if (!CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE) rdpmux_subsystem_thread, (void *) system, 0, NULL))
-        return -1;
-
-    return 1;
-}
-
-int rdpmux_subsystem_stop(rdpmuxShadowSubsystem *system)
-{
-    WLog_INFO(TAG, "Stop rdpmux subsystem");
-    return 1;
 }
 
 int rdpmux_subsystem_enum_monitors(MONITOR_DEF *monitors, int maxMonitors)
@@ -195,6 +126,7 @@ int rdpmux_subsystem_enum_monitors(MONITOR_DEF *monitors, int maxMonitors)
     int numMonitors = 1;
     MONITOR_DEF *monitor = &monitors[0];
 
+    /* default to largest size allowed */
     monitor->left = 0;
     monitor->top = 0;
     monitor->right = 4096;
@@ -204,6 +136,35 @@ int rdpmux_subsystem_enum_monitors(MONITOR_DEF *monitors, int maxMonitors)
     return numMonitors;
 }
 
+BOOL rdpmux_subsystem_check_resize(rdpmuxShadowSubsystem *system)
+{
+    if (system->src_width != system->listener->GetWidth() || system->src_height != system->listener->GetHeight()) {
+        /* screen size changed */
+        MONITOR_DEF *monitor = &(system->monitors[0]);
+        MONITOR_DEF *virtualScreen = &(system->virtualScreen);
+
+        /* update monitor definition */
+        monitor->left = 0;
+        monitor->left = 0;
+        monitor->bottom = system->listener->GetHeight();
+        monitor->right = system->listener->GetWidth();
+
+        /* resize */
+        shadow_screen_resize(system->server->screen);
+        system->src_height = system->listener->GetHeight();
+        system->src_width = system->listener->GetWidth();
+
+        /* update virtual screen */
+        virtualScreen->top = 0;
+        virtualScreen->left = 0;
+        virtualScreen->bottom = system->src_height;
+        virtualScreen->right = system->src_width;
+        virtualScreen->flags = 1;
+        return TRUE;
+    }
+    return FALSE;
+}
+
 int rdpmux_subsystem_init(rdpmuxShadowSubsystem *system)
 {
     system->numMonitors = rdpmux_subsystem_enum_monitors(system->monitors, 1);
@@ -211,11 +172,14 @@ int rdpmux_subsystem_init(rdpmuxShadowSubsystem *system)
 
     virtualScreen->left = 0;
     virtualScreen->top = 0;
-    virtualScreen->right = 1024;
-    virtualScreen->bottom = 768;
+    virtualScreen->right = system->listener->GetWidth();
+    virtualScreen->bottom = system->listener->GetHeight();
     virtualScreen->flags = 1;
 
-    printf("subsystem selectedMonitor is %d\n", system->selectedMonitor);
+    system->src_height = system->listener->GetHeight();
+    system->src_width = system->listener->GetWidth();
+
+    WLog_DBG("subsystem selectedMonitor is %d\n", system->selectedMonitor);
 
     return 1;
 }
@@ -246,6 +210,70 @@ rdpmuxShadowSubsystem *rdpmux_subsystem_new()
 void rdpmux_subsystem_free(rdpmuxShadowSubsystem *system)
 {
     free(system);
+}
+
+void *rdpmux_subsystem_thread(rdpmuxShadowSubsystem *system)
+{
+    DWORD nCount = 0;
+    DWORD status;
+    HANDLE events[32];
+    HANDLE stopEvent = system->server->StopEvent;
+    wMessagePipe *msgPipe = system->MsgPipe;
+    wMessage message;
+    DWORD interval;
+    UINT64 frametime;
+
+    events[nCount++] = stopEvent;
+    events[nCount++] = MessageQueue_Event(msgPipe->In);
+
+    system->captureFrameRate = 30;
+    interval = (DWORD) (1000 / system->captureFrameRate);
+    frametime = GetTickCount64() + interval;
+
+    while(1) {
+        status = WaitForMultipleObjects(nCount, events, FALSE, 0);
+
+        if (WaitForSingleObject(stopEvent, 0) == WAIT_OBJECT_0) {
+            WLog_INFO(TAG, "Server is stopping");
+            break;
+        }
+
+        if (WaitForSingleObject(MessageQueue_Event(msgPipe->In), 0) == WAIT_OBJECT_0) {
+            if (MessageQueue_Peek(msgPipe->In, &message, TRUE)) {
+                if (message.id == WMQ_QUIT) {
+                    break;
+                }
+                rdpmux_subsystem_process_message(system, &message);
+            }
+        }
+
+        if (status == WAIT_TIMEOUT || GetTickCount64() > frametime) {
+            rdpmux_subsystem_check_resize(system);
+            rdpmux_subsystem_update_frame(system, FALSE);
+            interval = 1000 / system->captureFrameRate;
+            frametime += interval;
+        }
+    }
+
+    return NULL;
+}
+
+
+int rdpmux_subsystem_start(rdpmuxShadowSubsystem *system)
+{
+    if (!system)
+        return -1;
+
+    if (!CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE) rdpmux_subsystem_thread, (void *) system, 0, NULL))
+        return -1;
+
+    return 1;
+}
+
+int rdpmux_subsystem_stop(rdpmuxShadowSubsystem *system)
+{
+    WLog_DBG(TAG, "Stop rdpmux subsystem");
+    return 1;
 }
 
 FREERDP_API int RDPMux_ShadowSubsystemEntry(RDP_SHADOW_ENTRY_POINTS *pEntryPoints)
